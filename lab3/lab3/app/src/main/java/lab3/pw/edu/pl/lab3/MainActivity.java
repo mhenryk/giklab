820cc3e0b3c6870a2954e5a6124d1cbeda06e561
Mateusz Masny <m.h.masny@gmail.com>
2018-03-14 08:32:35 +0100

    Lab 4 readme

--- lab3/lab3/app/src/main/java/lab3/pw/edu/pl/lab3/MainActivity.java
+++ lab3/lab3/app/src/main/java/lab3/pw/edu/pl/lab3/MainActivity.java
@@ -1,14 +1,38 @@
 package lab3.pw.edu.pl.lab3;
 
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private LocationCallback callback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult.getLastLocation() != null) {
+                Log.d(TAG, "Update " + locationResult.getLastLocation().getLongitude() + ", " + locationResult.getLastLocation().getLatitude());
+            }
+        }
+    };
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        F
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        LocationRequest request = new LocationRequest();
+        request.setInterval(30000);
+        client.requestLocationUpdates(request, callback, null);
     }
 }
---
+++ lab4/README.md
@@ -0,0 +1,15 @@
+# Laboratorium 3
+
+## Mapy ##
+
+### Zadanie 1 ###
+
+Utworzyć projekt w konsoli Google, dodać dostęp do api map
+
+### Zadanie 2 ###
+
+Dodać mapę do projektu
+
+### Zadanie 3 ###
+
+Dodać wyświetlanie aktualnej lokalizacji na mapie z update-ami zgodnie z poprzednim lab.
